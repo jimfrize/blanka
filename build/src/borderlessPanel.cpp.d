cc33489bb1d48d14307b3297fb551e4b8a389f7d
Jim Frize <jamesfrize@gmail.com>
2022-03-05 17:00:39 +0000

    Add files via upload

---
+++ build/src/borderlessPanel.cpp.d
@@ -0,0 +1,82 @@
+build/src/borderlessPanel.cpp.o: src/borderlessPanel.cpp \
+  /Users/jim/Desktop/Rack-SDK/include/app/SvgPanel.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/app/common.hpp \
+  /Users/jim/Desktop/Rack-SDK/dep/include/jansson.h \
+  /Users/jim/Desktop/Rack-SDK/dep/include/jansson_config.h \
+  /Users/jim/Desktop/Rack-SDK/include/common.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/logger.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/math.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/ui/common.hpp \
+  /Users/jim/Desktop/Rack-SDK/dep/include/nanovg.h \
+  /Users/jim/Desktop/Rack-SDK/dep/include/blendish.h \
+  /Users/jim/Desktop/Rack-SDK/include/color.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/string.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/widget/TransparentWidget.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/widget/Widget.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/window/Window.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/window/Svg.hpp \
+  /Users/jim/Desktop/Rack-SDK/dep/include/nanosvg.h \
+  /Users/jim/Desktop/Rack-SDK/dep/include/GL/glew.h \
+  /Users/jim/Desktop/Rack-SDK/dep/include/GLFW/glfw3.h \
+  /Users/jim/Desktop/Rack-SDK/dep/include/nanovg_gl.h \
+  /Users/jim/Desktop/Rack-SDK/dep/include/nanovg_gl_utils.h \
+  /Users/jim/Desktop/Rack-SDK/include/widget/event.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/weakptr.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/widget/FramebufferWidget.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/widget/SvgWidget.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/context.hpp \
+  /Users/jim/Desktop/Rack-SDK/include/settings.hpp
+
+/Users/jim/Desktop/Rack-SDK/include/app/SvgPanel.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/app/common.hpp:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/jansson.h:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/jansson_config.h:
+
+/Users/jim/Desktop/Rack-SDK/include/common.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/logger.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/math.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/ui/common.hpp:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/nanovg.h:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/blendish.h:
+
+/Users/jim/Desktop/Rack-SDK/include/color.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/string.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/widget/TransparentWidget.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/widget/Widget.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/window/Window.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/window/Svg.hpp:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/nanosvg.h:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/GL/glew.h:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/GLFW/glfw3.h:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/nanovg_gl.h:
+
+/Users/jim/Desktop/Rack-SDK/dep/include/nanovg_gl_utils.h:
+
+/Users/jim/Desktop/Rack-SDK/include/widget/event.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/weakptr.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/widget/FramebufferWidget.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/widget/SvgWidget.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/context.hpp:
+
+/Users/jim/Desktop/Rack-SDK/include/settings.hpp:
--- src/4hp.cpp
+++ src/4hp.cpp
@@ -1,128 +1,137 @@
 #include "plugin.hpp"
 	
-	struct _4hp : Module
+struct _4hp : Module
+{
+	enum ParamId
 	{
-		enum ParamId
-		{
-			PARAMS_LEN
-		};
-		enum InputId
-		{
-			INPUTS_LEN
-		};
-		enum OutputId
-		{
-			OUTPUTS_LEN
-		};
-		enum LightId
-		{
-			LIGHTS_LEN
-		};
-	
-		_4hp()
-		{
-			config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		}
-	
-		std::string jsonSavePath; // storage for SVG image file path
-		bool pathDirty = false; // flag to indicate path has been changed
-	
-		// save and load filepath to JSON file
-	
-		json_t *dataToJson() override 
-		{
-			json_t *rootJ = json_object();
-			json_object_set_new(rootJ, "sPATH", json_string(jsonSavePath.c_str()));	
-			return rootJ;
-		}
-	
-		void dataFromJson(json_t *rootJ) override
-		{
-			json_t *savedPATHJ = json_object_get(rootJ, "sPATH");
-			if (savedPATHJ) {
-				jsonSavePath = json_string_value(savedPATHJ);
-				pathDirty = true;
-			}
-		}
+		PARAMS_LEN
 	};
-	
-	struct customPanel : SvgWidget
+	enum InputId
 	{
-		_4hp *module;
-		customPanel()
-		{
-			// Does nothing because PATH is empty on construction
-			//	setSvg(Svg::load(asset::plugin(pluginInstance, PATH))); // set SVG file on creation of customPanel
-		}
-	
-		void draw(const DrawArgs& args) override
-		{
-			if (module) {
-				if (module->pathDirty) {
-					module->pathDirty = false;
-					setSvg(Svg::load(asset::plugin(pluginInstance, module->jsonSavePath))); // set new file path
-				}
-			}
-	
-			SvgWidget::draw(args); // continue to run the rest of the default draw function
-		}
+		INPUTS_LEN
 	};
-	
-	struct dnd : TransparentWidget
+	enum OutputId
 	{
-		_4hp *module;
-		std::string svgPath = asset::plugin(pluginInstance, "res/default.svg"); // path with SVG extension
-	
-		void onPathDrop(const PathDropEvent &e) override
+		OUTPUTS_LEN
+	};
+	enum LightId
+	{
+		LIGHTS_LEN
+	};
+
+	_4hp()
+	{
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+	}
+
+	/////////////////////////////////////////
+	// save and load filepath to JSON file //
+	/////////////////////////////////////////
+
+	std::string svgSavePath; // storage for SVG image file path
+	bool pathChange = false; // flag to indicate path has changed
+
+	json_t *dataToJson() override 
+	{
+		json_t *rootJ = json_object();
+		json_object_set_new(rootJ, "sPATH", json_string(svgSavePath.c_str()));	
+		return rootJ;
+	}
+
+	void dataFromJson(json_t *rootJ) override
+	{
+		json_t *savedPATHJ = json_object_get(rootJ, "sPATH");
+
+		if (savedPATHJ) 
 		{
-			std::string filePath = e.paths.front(); // get first path in array, all other paths are ignored
-	
-			if(filePath.substr(filePath.find_last_of(".") + 1) == "svg") // check for SVG extension
+			svgSavePath = json_string_value(savedPATHJ);
+			pathChange = true;
+		}
+	}
+};
+
+struct customPanel : SvgWidget
+{
+	_4hp *module; // pointer to module
+	std::string svgPath = asset::plugin(pluginInstance, "res/default.svg"); // default image
+
+	customPanel()
+	{
+		setSvg(Svg::load(svgPath));
+	}
+
+	void onPathDrop(const PathDropEvent &e) override
+	{
+		std::string filePath = e.paths.front(); // get first path in array, all other paths are ignored
+
+		if(filePath.substr(filePath.find_last_of(".") + 1) == "svg") // check for SVG extension
+		{
+			svgPath = filePath; // store current path
+
+			if (module) // check for module
 			{
-				svgPath = filePath; // store path
-				if (module) {
-					module->jsonSavePath = svgPath;
-					module->pathDirty = true;
-				}
-			}
-	
-			else
-			{
-				DEBUG("Invalid file extension: try *.svg\n");
+				module->svgSavePath = svgPath;
+				module->pathChange = true;
 			}
 		}
-	};
-	
-	struct _4hpWidget : ModuleWidget
-	{
-		_4hpWidget(_4hp *module)
+
+		else
 		{
-			setModule(module);
-			setPanel(createPanel(asset::plugin(pluginInstance, "res/4hp.svg")));
-	
-			// maunally add widgets to allow for pointer references
-	
-			dnd *DND = new dnd();
-			DND->module = module;
-			DND->box.pos = Vec(0, 0);
-			DND->box.size = Vec(RACK_GRID_WIDTH * 4, RACK_GRID_HEIGHT);
-			addChild(DND);
-	
-			customPanel *CP = new customPanel();
-			CP->module = module;
-			CP->box.pos = Vec(0, 0);
-			CP->box.size = Vec(RACK_GRID_WIDTH * 4, RACK_GRID_HEIGHT);
-			addChild(CP);
-	
-			if (module)
+			DEBUG("Invalid file extension: try *.svg\n");
+		}
+	}
+
+	void draw(const DrawArgs& args) override
+	{
+		if (module)
+		{
+			if (module->pathChange) 
 			{
-				if (!module->pathDirty) {
-					module->jsonSavePath = DND->svgPath;
-					module->pathDirty = true;
-				}
-				DEBUG("%s\n", module->jsonSavePath.c_str());
+				setSvg(Svg::load(asset::plugin(pluginInstance, module->svgSavePath))); // set new file path
+				
+				module->pathChange = false;
 			}
 		}
-	};
-	
-	Model* model_4hp = createModel<_4hp, _4hpWidget>("4hp");
+
+		SvgWidget::draw(args); // continue to run the rest of the default draw function
+	}
+};
+
+struct _4hpWidget : ModuleWidget
+{
+	_4hpWidget(_4hp *module)
+	{
+		setModule(module);
+
+		// manually create panel and set hidden to hide grey border
+		SvgPanel *borderlessPanel = new SvgPanel();
+		borderlessPanel->setBackground(Svg::load(asset::plugin(pluginInstance, "res/4hp.svg")));
+		borderlessPanel->hide();
+		setPanel(borderlessPanel);
+
+		// maunally add widget to allow for pointer reference
+		customPanel *CP = new customPanel();
+		CP->module = module;
+		CP->box.pos = Vec(0, 0);
+		CP->box.size = Vec(RACK_GRID_WIDTH * 4, RACK_GRID_HEIGHT);
+		addChild(CP);
+
+		if (module)
+		{
+			if (!module->pathChange) 
+			{
+				module->svgSavePath = CP->svgPath;
+				module->pathChange = true;
+			}
+
+			DEBUG("%s\n", module->svgSavePath.c_str());
+		}
+	}
+
+	void drawLayer(const DrawArgs& args, int layer) override // override drawing module dropshadow
+	{
+		// do nothing
+	}
+};
+
+Model *model_4hp = createModel<_4hp, _4hpWidget>("4hp");
